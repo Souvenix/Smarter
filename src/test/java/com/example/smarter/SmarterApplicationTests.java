ec2af45a75c9408fe06834c4d10147ce335d9af7
Mazai-Liu <2510382609@qq.com>
2025-04-13 16:19:24 +0800

    init: gitignore配置-

---
+++ src/test/java/com/example/smarter/SmarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
